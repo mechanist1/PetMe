c334bc7b35f7a2a7bba658d1ff60bd4612c4ecb6
mechanist1 <mlaouhiilyess@gmail.com>
2023-01-10 17:20:00 +0100

    last

--- projet semestriel final/website/views.py
+++ projet semestriel final/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import Form,User
 from sqlalchemy import desc
